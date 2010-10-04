55b6c4939eee161d824654d9be37821ccc03270d
emerson <emerson@dbadmin.corp.causes.com>
2010-10-04 16:24:05 -0700

    Fix up a couple places where "User" should be .to_sentence Fix people not being pre-highlighted if you "Give X a star!"

--- app/controllers/seconds_controller.rb
+++ app/controllers/seconds_controller.rb
@@ -6,7 +6,7 @@ class SecondsController < ApplicationController
     @star = Star.find(params[:star_id])
     if current_user.can_second?(@star)
       current_user.second(@star)
-      flash[:notice] = "You seconded #{@star.to.name} for '#{truncate(@star.reason, 60, '...')}'"
+      flash[:notice] = "You seconded #{@star.to_sentence} for '#{truncate(@star.reason, 60, '...')}'"
     end
   rescue ActiveRecord::RecordNotFound
     flash[:notice] = "That star doesn't exist."
