c3cc94e344abdd832eef2e8a5714350acabdc3d2
Kristján Pétursson <kristjan@gmail.com>
2012-02-27 00:10:55 -0800

    Fix user list when there's no one inactive
    It was referencing a nil. Now it's always an array.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :stars
   has_and_belongs_to_many :teams
 
-  named_scope :active, :conditions => {:active => true}
+  named_scope :active,   :conditions => {:active => true}
+  named_scope :inactive, :conditions => {:active => false}
 
   def can_second?(star)
     return false if [star.from, star.to].flatten.include?(self)
